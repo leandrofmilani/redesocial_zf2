16cdaab947e2b485a0901e1c6a00402c69cca296
Leandro Fabris Milani <leandrofmilani@gmail.com>
2015-06-15 10:26:37 -0300

    removido arquivos invalidos

--- module/Main/config/module.config.php
+++ module/Main/config/module.config.php
@@ -131,7 +131,6 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'main/index/index' => __DIR__ . '/../view/main/index/index.phtml',
-            'main/index/pesq' => __DIR__ . '/../view/main/pesq/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
